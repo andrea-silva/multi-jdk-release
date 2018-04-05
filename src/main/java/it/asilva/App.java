92355cb3e5f146ca373df2032f53f16b7c56ac26
bardamu-2 <andrea.silva@almazsa.com>
2018-04-05 22:26:44 +0200

    initial commit

---
+++ src/main/java/it/asilva/App.java
@@ -0,0 +1,17 @@
+package it.asilva;
+
+
+import java.util.Collections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Collections.emptyMap();
+        System.out.println( "Hello World!" );
+    }
+}
